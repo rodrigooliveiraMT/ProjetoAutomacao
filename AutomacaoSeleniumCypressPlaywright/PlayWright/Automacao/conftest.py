137a57dbb3e30a9334de891528320e09eb24aefc
rodrigooliveiraMT <oliveirarodrigocr@gmail.com>
2026-07-31 10:06:23 -0400

    Alteração no projeto Playwright

--- AutomacaoSeleniumCypressPlaywright/PlayWright/Automacao/conftest.py
+++ AutomacaoSeleniumCypressPlaywright/PlayWright/Automacao/conftest.py
@@ -1,35 +1,17 @@
 import pytest
-from playwright.sync_api import sync_playwright
-
-def pytest_sessionstart(session):
-    print("=== INÍCIO DA EXECUÇÃO ===")
-
-
-def pytest_sessionfinish(session, exitstatus):
-    print("\n=== FIM DA EXECUÇÃO ===")
-
 
 @pytest.fixture(scope="session")
-def browser():
-    playwright = sync_playwright().start()
-    browser = playwright.chromium.launch(
-        headless=False,
-        args=["--start-maximized"]
-    )
-    yield browser
-    browser.close()
-    playwright.stop()
+def browser_type_launch_args():
+    return {
+        "headless": False,
+        "args": ["--start-maximized"]
+    }
 
-
-@pytest.fixture(scope="function")
-def page(browser):
-    context = browser.new_context(
-        no_viewport=True,
-        record_video_dir="Vídeos"
-        # record_video_size={"width": 1280, "height": 720}
-    )
-    page = context.new_page()
-    page.set_default_timeout(25000)
-    page.set_default_navigation_timeout(50000)
-    yield page
-    context.close()
+@pytest.fixture(scope="session")
+def browser_context_args(browser_context_args):
+    return {
+        **browser_context_args,
+        "no_viewport": True # Abre o navegador em tela cheia
+                # record_video_dir="videos" # Grava vídeos da execução dos testes
+                # record_video_size={"width": 1280, "height": 720} # Define o tamanho do vídeo (opcional)
+    }
